778fc0a74cda54faa75c84156daf491fc15db827
SLAVKO&VESNA <SLAVKO&VESNA@DESKTOP-560C2LD>
2017-02-02 14:04:54 +0100

    New button

--- Smth/src/Graphics/Display.java
+++ Smth/src/Graphics/Display.java
@@ -1,20 +1,27 @@
 package Graphics;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Display {
-	
+
 	public static void Display() {
 
-		//Basic Frame
+		// JButton
+		JButton button1 = new JButton();
+		button1.setBounds(0, 0, 10, 10);
+
+		// Basic Frame
 		JFrame f = new JFrame("Test");
 		f.setSize(800, 600);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLocationRelativeTo(null);
 		f.setResizable(false);
+		contentPane.add(button1);
+		f.add(button1);
+
 		f.setVisible(true);
-		
-		
+
 	}
 
 }
