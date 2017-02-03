dea10ec7e3e271ae07598f6f033933b8d8e2b11a
SLAVKO&VESNA <SLAVKO&VESNA@DESKTOP-560C2LD>
2017-02-03 16:01:46 +0100

    Fixed buttons! - Works now

--- Smth/src/Graphics/Display.java
+++ Smth/src/Graphics/Display.java
@@ -5,11 +5,7 @@ import javax.swing.JFrame;
 
 public class Display {
 
-	public static void Display() {
-
-		// JButton
-		JButton button1 = new JButton();
-		button1.setBounds(0, 0, 10, 10);
+	public static void Display() {		
 
 		// Basic Frame
 		JFrame f = new JFrame("Test");
@@ -17,8 +13,25 @@ public class Display {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLocationRelativeTo(null);
 		f.setResizable(false);
-		contentPane.add(button1);
-		f.add(button1);
+		
+		f.setLayout(null);
+		
+		// JButton
+		JButton b1 = new JButton();
+		JButton b2 = new JButton();
+		JButton b3 = new JButton();
+		JButton b4 = new JButton();
+		b1.setBounds(10, 30, 150, 100);
+		b2.setBounds(10, 150, 150, 100);
+		b3.setBounds(10, 270, 150, 100);
+		b4.setBounds(10, 390, 150, 100);
+		
+		f.add(b1);
+		f.add(b2);
+		f.add(b3);
+		f.add(b4);
+		
+		
 
 		f.setVisible(true);
 
